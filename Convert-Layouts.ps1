61ff73c7e66e20b538f60a529507c73307600982
Sly Gryphon <sly.gryphon@readify.net>
2020-02-18 20:44:44 +1000

    Without region version of style cop layout

--- Convert-Layouts.ps1
+++ Convert-Layouts.ps1
@@ -1,5 +1,24 @@
 ﻿Write-Host 'Converting layouts with regions to without'
 
+# Style Cop Layout
+$regionFile = 'csharp-layout-stylecop+regions.xml'
+$plainFile = 'csharp-layout-stylecop.xml'
+
+$regionPath = Join-Path $PSScriptRoot $regionFile;
+$layoutXml = [xml](Get-Content -Path $regionPath);
+
+foreach ($typePattern in $layoutXml.Patterns.TypePattern) {
+    foreach ($region in $typePattern.Region) {
+        foreach ($entry in $region.Entry) {
+            $typePattern.AppendChild($entry);
+        }
+        $typePattern.RemoveChild($region);
+    }
+}
+
+$plainPath = Join-Path $PSScriptRoot $plainFile;
+$layoutXml.Save($plainPath);
+
 # Documentation Layout
 $regionFile = 'csharp-layout-documentation+regions.xml'
 $plainFile = 'csharp-layout-documentation.xml'
